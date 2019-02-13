0f522854962c71e0abed7262779f94c3f3c3e72c
nadamjawad98 <g00068826@aus.edu>
2019-02-13 17:50:55 +0400

    Initial Commit

---
+++ src/coe420lab2/COE420lab2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420lab2;
+
+/**
+ *
+ * @author g00068826
+ */
+public class COE420lab2 {
+    
+    public static void addFeatureA(){
+        System.out.println("Feature A added");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        addFeatureA();
+        // TODO code application logic here
+    }
+    
+}
